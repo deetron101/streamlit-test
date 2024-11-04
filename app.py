ec45e2a5c3ed4ac5259b13c5a0911f31ff43c860
Deirdre O'Regan <deirdre.oregan@gmail.com>
2024-11-04 23:23:01 +0000

    More testing streamlit
    More testing streamlit

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+from langchain_aws import ChatBedrock
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
-st.title("Helld")
+template = (
+    """You are a really nice guy. Answer the user's question in a friendly tone"""
+)
 
-st.text_input("This is an input")
+prompt = ChatPromptTemplate.from_template(template)
+
+# Set up the Streamlit framework
+st.title("AI Assistant")  # Set the title of the Streamlit app
+input_text = st.text_input(
+    "Ask your question!"
+)  # Create a text input field in the Streamlit app
